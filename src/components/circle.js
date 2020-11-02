cbae35db6c0b560f7fea8e02922f60d59f42b9cf
Mohit Gehi <47024623+mohitgehi@users.noreply.github.com>
2020-11-02 23:57:33 +0530

    commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,10 @@
 
 // declare class
 
-
 // export class using module.exports
+import Shape from "./shape";
+
+class Circle extends Shape {
+  calculateArea() {}
+}
+export default Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
 // export class using module.exports
+
+import React, { Component } from "react";
+
+class Shape extends React.Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.color = "red";
+    this.drawShape = this.drawShape.bind(this);
+    this.calculateArea = this.calculateArea.bind(this);
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+
+export default Shape;
